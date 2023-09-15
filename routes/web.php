cca94ce1cc34627dad9ad3f28c20ffa969da6896
jowel007 <abuhanifjowel1@gmail.com>
2023-09-16 00:18:28 +0600

    user profile logout option

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::get('/user/profile', [UserController::class, 'UserProfile'])->name('user.profile');
     Route::post('/user/profile/store', [UserController::class, 'UserProfileStore'])->name('user.profile.store');
+    Route::get('/user/logout', [UserController::class, 'UserLogout'])->name('user.logout');
 
     // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
