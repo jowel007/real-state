cefe43047019c54797e674f25f2bc4b4506718bf
jowel007 <abuhanifjowel1@gmail.com>
2024-01-06 23:53:34 +0600

    property details & delete property from agent dashboard part - V

--- routes/web.php
+++ routes/web.php
@@ -165,7 +165,9 @@ Route::middleware(['auth','role:agent'])->group(function (){
 
         Route::post('/agent/update/property/facilities', 'AgentUpdatePropertyFacilities')->name('agent.update.property.facilities');
 
-        
+        Route::get('/agent/details/property/{id}', 'AgentDetailsProperty')->name('agent.details.property'); 
+
+        Route::get('/agent/delete/property/{id}', 'AgentDeleteProperty')->name('agent.delete.property'); 
         
     });
 
