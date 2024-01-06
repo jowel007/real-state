2fe17070b371023930feb44fc56e0ef0eabacf42
jowel007 <abuhanifjowel1@gmail.com>
2024-01-06 12:15:37 +0600

    add or create agent in admin side part - II

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -123,6 +123,36 @@ class AdminController extends Controller
     
       }// End Method 
 
+    public function AddAgent(){
+
+        return view('backend.agentuser.add_agent');
+    
+    }// End Method
+
+
+    public function StoreAgent(Request $request){
+
+        User::insert([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'password' => Hash::make($request->password),
+            'role' => 'agent',
+            'status' => 'active', 
+        ]);
+    
+    
+           $notification = array(
+                'message' => 'Agent Created Successfully',
+                'alert-type' => 'success'
+            );
+    
+            return redirect()->route('all.agent')->with($notification); 
+    
+    
+      }// End Method 
+
 
 
 }
