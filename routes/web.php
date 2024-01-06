47ffe2024d97988efe7a6401f77046eb8616465a
jowel007 <abuhanifjowel1@gmail.com>
2024-01-06 11:43:11 +0600

    agent profile image & data update

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,8 @@ Route::middleware(['auth','role:agent'])->group(function (){
 Route::get('/agent/login', [AgentController::class, 'AgentLogin'])->name('agent.login')->middleware(RedirectIfAuthenticated::class);
 Route::post('/agent/register', [AgentController::class, 'AgentRegister'])->name('agent.register');
 Route::get('/agent/logout', [AgentController::class, 'AgentLogout'])->name('agent.logout');
+Route::get('/agent/profile', [AgentController::class, 'AgentProfile'])->name('agent.profile');
+Route::post('/agent/profile/store', [AgentController::class, 'AgentProfileStore'])->name('agent.profile.store');
 
 Route::middleware(['auth','role:admin'])->group(function (){
 
