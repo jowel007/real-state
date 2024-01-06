ad97665fa9b93fe6080f7164742bcccc48c9c4fb
jowel007 <abuhanifjowel1@gmail.com>
2024-01-06 11:47:58 +0600

    agent profile change password & update

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,8 @@ Route::post('/agent/register', [AgentController::class, 'AgentRegister'])->name(
 Route::get('/agent/logout', [AgentController::class, 'AgentLogout'])->name('agent.logout');
 Route::get('/agent/profile', [AgentController::class, 'AgentProfile'])->name('agent.profile');
 Route::post('/agent/profile/store', [AgentController::class, 'AgentProfileStore'])->name('agent.profile.store');
+Route::get('/agent/change/password', [AgentController::class, 'AgentChangePassword'])->name('agent.change.password');
+Route::post('/agent/update/password', [AgentController::class, 'AgentUpdatePassword'])->name('agent.update.password');
 
 Route::middleware(['auth','role:admin'])->group(function (){
 
