0a49164f499b49758637f151d60348655426cd7f
jowel007 <abuhanifjowel1@gmail.com>
2024-03-24 21:14:34 +0600

    dynamic email configuration - III

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\SmtpSettings;
+use Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (\Schema::hasTable('smtp_settings')) {
+
+            $smtpsetting = SmtpSettings::first();
+
+            if ($smtpsetting) {
+                $data = [
+
+                    'driver' => $smtpsetting->mailer,
+                    'host' => $smtpsetting->host,
+                    'port' => $smtpsetting->post,
+                    'username' => $smtpsetting->username,
+                    'password' => $smtpsetting->password,
+                    'encryption' => $smtpsetting->encryption, 
+                    'from' => [
+                        'address' => $smtpsetting->from_address,
+                        'name' => 'AB Builders'
+                    ]
+                ];
+                Config::set('mail',$data);
+            }
+
+        }   // End If
     }
 }
